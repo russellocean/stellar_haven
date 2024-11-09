40a5f237c94c53a2cf3a564686b34a9b9cf4d9f4
russellocean <russellwelch17@gmail.com>
2024-11-09 17:30:05 -0500

    make quitting functional

--- src/main.py
+++ src/main.py
@@ -10,12 +10,14 @@ def main():
     clock = pygame.time.Clock()
     game = Game(screen)
 
-    while True:
+    while game.running:
         game.handle_events()
         game.update()
         game.draw()
         pygame.display.flip()
         clock.tick(60)
+
+    pygame.quit()
 
 
 if __name__ == "__main__":

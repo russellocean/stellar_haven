292ca530dac80a06791be0f2c7064e3a3a56eee9
russellocean <russellwelch17@gmail.com>
2024-11-09 18:01:22 -0500

    On prologue completetion switch to game

--- src/game.py
+++ src/game.py
@@ -4,6 +4,7 @@ from scenes.gameplay import GameplayScene
 from scenes.menu_scene import MenuScene
 from scenes.pause_scene import PauseScene
 from scenes.prologue_scene import PrologueScene
+from systems.event_system import EventSystem, GameEvent
 from systems.scene_manager import SceneManager, SceneType
 
 
@@ -12,6 +13,12 @@ class Game:
         self.screen = screen
         self.running = True
         self.scene_manager = SceneManager()
+        self.event_system = EventSystem()
+
+        # Subscribe to game state changes
+        self.event_system.subscribe(
+            GameEvent.GAME_STATE_CHANGED, self._on_game_state_changed
+        )
 
         # Initialize scenes
         self.scene_manager.add_scene(SceneType.MAIN_MENU, MenuScene(self))
@@ -21,6 +28,17 @@ class Game:
 
         # Start with main menu
         self.scene_manager.set_scene(SceneType.MAIN_MENU)
+
+    def _on_game_state_changed(self, event_data):
+        """Handle game state changes"""
+        new_state = event_data.data.get("new_state")
+        if new_state:
+            try:
+                # Convert string to SceneType enum
+                scene_type = SceneType[new_state]
+                self.scene_manager.set_scene(scene_type)
+            except KeyError:
+                print(f"Warning: Invalid scene type {new_state}")
 
     def handle_events(self):
         for event in pygame.event.get():
